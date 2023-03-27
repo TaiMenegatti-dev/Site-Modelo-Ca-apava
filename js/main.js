be724361b1d9cbab6854f3c1379e4851a4245215
TaiMenegatti-dev <tainam.vieira@gmail.com>
2023-03-27 14:41:05 -0300

    alteracao de arquivos

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function mapaMental(){
+    alert("Redirecionando para mapa mental sobre Fuso Horário.");
+    window.location.href = "https://drive.google.com/file/d/1OPkP_k35tJqZvd-tf78XzpKZa6gqA2ef/preview"
+}
+
+function MapaMental(){
+    alert("Redirecionando para mapa mental sobre Fluxo Migratório.");
+    window.location.href = "https://drive.google.com/file/d/1XjUP38FCQTRzXoIYdifBC9M8JCEPtFf_/preview"
+}
